0dddb539b446b7eb03f573a827e6ec46719d740d
Goofy_Ozy4 <Ozy4David@gmail.com>
2025-01-18 17:57:19 +0200

    Update startup.sh

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 wal --saturate 0.33 -i $HOME/Wallpaper/Wallpaper.png
-# Symlink dunst config
-ln -sf ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
-# Restart dunst with the new color scheme
-pkill dunst
-dunst &
-
+sh ~/.config/dunst/gen.sh
 sleep 0.3
 polybar
